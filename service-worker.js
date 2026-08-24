7e5abb2f60ea045f356eb6c22e3ff58bc344e88e
Mabel00-creator <lihongdanddd@gmail.com>
2026-08-24 10:51:32 +0800

    Update icon paths in APP_SHELL array

--- service-worker.js
+++ service-worker.js
@@ -4,10 +4,10 @@ const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png",
-  "./icons/icon-maskable-512.png",
-  "./icons/apple-touch-icon.png"
+  "./icons-192.png",
+  "./icons-512.png",
+  "./icons-maskable-512.png",
+  "./apple-touch-icon.png"
 ];
 
 self.addEventListener("install", event => {
